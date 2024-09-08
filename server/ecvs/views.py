d9ed3e6c65d06560a84b3a536de19ba7001b5426
addymwenda12 <muneneaddy@gmail.com>
2024-09-08 16:40:34 +0300

    Modified views to handle API requests

--- server/ecvs/views.py
+++ server/ecvs/views.py
@@ -1,37 +1,12 @@
-from django.shortcuts import render, redirect
+from rest_framework import viewsets
 from .models import Credential
-from .forms import CredentialForm
+from .serializers import CredentialSerializer
 """
-Views for the ECVS application.
+ViewSets for the Credential model.
 """
-
-def create_credential(request):
+class CredentialViewSet(viewsets.ModelViewSet):
     """
-    View to create a new credential.
+    ViewSet for the Credential model.
     """
-    if request.method == 'POST':
-        form = CredentialForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('credential_list')
-    else:
-        form = CredentialForm()
-    return render(request, 'create_credential.html', {'form': form})
-
-def credential_list(request):
-    """
-    View to list all credentials.
-    """
-    credentials = Credential.objects.all()
-    return render(request, 'credential_list.html', {'credentials': credentials})
-
-def verify_credential(request, credential_id):
-    """
-    View to verify a credential by ID.
-    """
-    try:
-        credential = Credential.objects.get(credential_id=credential_id)
-        is_verified = credential.verify()
-        return render(request, 'verify_credential.html', {'credential': credential, 'is_verified': is_verified})
-    except Credential.DoesNotExist:
-        return render(request, 'verify_credential.html', {'error': 'Credential not found'})
+    queryset = Credential.objects.all()
+    serializer_class = CredentialSerializer
