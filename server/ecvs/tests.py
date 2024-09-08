9b7fa9a9c994078445b699746bc9e5f8b6f5ce1b
addymwenda12 <muneneaddy@gmail.com>
2024-09-08 17:23:24 +0300

    Updated tests for new functionalities

--- server/ecvs/tests.py
+++ server/ecvs/tests.py
@@ -29,3 +29,20 @@ class CredentialAPITest(TestCase):
         """
         response = self.client.get('/api/credentials/')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_update_credential(self):
+        """
+        Test the update of a credential.
+        """
+        self.client.post('/api/credentials/', self.credential_data, format='json')
+        updated_data = {"degree": "Master of Science"}
+        response = self.client.patch('/api/credentials/1/', updated_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_credential(self):
+        """
+        Test the deletion of a credential.
+        """
+        self.client.post('/api/credentials/', self.credential_data, format='json')
+        response = self.client.delete('/api/credentials/1/')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
