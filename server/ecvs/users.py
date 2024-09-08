d9caaa84a9ca2e3696200f28f2160434495ab270
addymwenda12 <muneneaddy@gmail.com>
2024-09-08 17:41:48 +0300

    Added User base model

--- server/ecvs/users.py
+++ server/ecvs/users.py
@@ -1,5 +1,35 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    """
+    Custom user model
+    """
+    ROLE_CHOICES = [
+        ('student', 'Student'),
+        ('employer', 'Employer'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    institution = models.CharField(max_length=100, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
 
 class UserProfile(models.Model):
+    """
+    User profile model
+    """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=[('student', 'Student'), ('employer', 'Employer')])
+    institution = models.CharField(max_length=100, blank=True, null=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
