be1cf10b854f73ab5c3f3b4fb58f449d17dde1b5
addymwenda12 <muneneaddy@gmail.com>
2024-09-08 17:33:58 +0300

    Created simple user model to manage roles

---
+++ server/ecvs/users.py
@@ -0,0 +1,5 @@
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=[('student', 'Student'), ('employer', 'Employer')])
