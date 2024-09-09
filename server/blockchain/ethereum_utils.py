8767501ea631ecf619da8b363d6102c074de977c
addymwenda12 <muneneaddy@gmail.com>
2024-09-09 16:00:33 +0300

    Implemented a basic Ethereum functionality

--- server/blockchain/ethereum_utils.py
+++ server/blockchain/ethereum_utils.py
@@ -5,3 +5,45 @@ import json
 
 load_dotenv()
 
+# Connect to the Ethereum network
+w3 = Web3(Web3.HTTPProvider(os.getenv('ETHEREUM_NODE_URL')))
+
+# Check if the connection is successful
+if not w3.isConnected():
+    raise Exception("Failed to connect to the Ethereum network")
+
+# Get the chain ID
+chain_id = w3.eth.chainId
+
+# Load contract ABI and address
+with open('server/blockchain/CredentialContract.json') as f:
+  contract_json = json.load(f)
+contract_abi = contract_json['abi']
+contract_address = os.getenv('CONTRACT_ADDRESS')
+
+# Create cotract instance
+credential_contract = w3.eth.contract(address=contract_address, abi=contract_abi)
+
+def issue_credential(credential_id, hash_value):
+    """
+    Issue a credential on the blockchain
+    """
+    account = w3.eth.account.from_key(os.getenv('PRIVATE_KEY'))
+    nonce = w3.eth.get_transaction_count(account.address)
+    
+    txn = credential_contract.functions.issueCredential(credential_id, hash_value).build_transaction({
+        'chainId': 1,  # Mainnet
+        'gas': 2000000,
+        'gasPrice': w3.eth.gas_price,
+        'nonce': nonce,
+    })
+
+    signed_txn = account.sign_transaction(txn)
+    tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+    return w3.eth.wait_for_transaction_receipt(tx_hash)
+
+def verify_credential(credential_id):
+    """
+    Verify a credential on the blockchain
+    """
+    return credential_contract.functions.verifyCredential(credential_id).call()
