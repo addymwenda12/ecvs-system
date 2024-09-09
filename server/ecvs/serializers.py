771313a25b0f7c22c155c2f606c27a767df9477d
addymwenda12 <muneneaddy@gmail.com>
2024-09-09 13:27:43 +0300

    Updated serilizer to implement creation of new credentials

--- server/ecvs/serializers.py
+++ server/ecvs/serializers.py
@@ -1,17 +1,22 @@
 from rest_framework import serializers
 from .models import Credential, User
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'role']
+
 class CredentialSerializer(serializers.ModelSerializer):
     """
-    Serializer for the Credential model.
+    Serializer for credential model
     """
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
+    user = UserSerializer(read_only=True)
+    user_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), source='user', write_only=True)
 
     class Meta:
         model = Credential
-        fields = '__all__'
+        fields = ['id', 'degree', 'institution', 'date_issued', 'credential_id', 'created_at', 'user', 'user_id', 'is_verified']
+        read_only_fields = ['id', 'created_at', 'is_verified']
 
-    def validate_user(self, value):
-        if not value:
-            raise serializers.ValidationError("User is required.")
-        return value
+    def create(self, validated_data):
+        return Credential.objects.create(**validated_data)
