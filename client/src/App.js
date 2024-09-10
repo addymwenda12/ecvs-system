1484946dd6748cb7007c4e5adff2013f2622b106
addymwenda12 <muneneaddy@gmail.com>
2024-09-10 15:17:36 +0300

    Added Login and Register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import AboutUs from './components/AboutUs';
@@ -7,19 +7,32 @@ import KeyFeatures from './components/KeyFeatures';
 import SkillTokenization from './components/SkillTokenization';
 import EmployerAccess from './components/EmployerAccess';
 import Testimonials from './components/Testimonials';
+import Login from './components/Login';
+import Register from './components/Register';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div>
-      <Header />
-      <Hero />
-      <AboutUs />
-      <HowItWorks />
-      <KeyFeatures />
-      <SkillTokenization />
-      <EmployerAccess />
-      <Testimonials />
+      <Header user={user} setUser={setUser} />
+      {user ? (
+        <Dashboard user={user} />
+      ) : (
+        <>
+          <Hero />
+          <AboutUs />
+          <HowItWorks />
+          <KeyFeatures />
+          <SkillTokenization />
+          <EmployerAccess />
+          <Testimonials />
+          <Login setUser={setUser} />
+          <Register setUser={setUser} />
+        </>
+      )}
     </div>
   );
 }
