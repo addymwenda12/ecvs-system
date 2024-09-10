2fc85ef641ca770add34da8b63726624e2192607
MichelleInnovates <azariahthesecond@gmail.com>
2024-09-10 14:50:28 +0300

    Initial Commit

---
+++ client/src/components/EmployerAccess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EmployerAccess = () => {
+  return (
+    <section className="employer-access">
+      <h2>Employer Access</h2>
+      <p>Verify candidates' qualifications easily and securely.</p>
+    </section>
+  );
+};
+
+export default EmployerAccess;
---
+++ client/src/components/KeyFeatures.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const KeyFeatures = () => {
+  return (
+    <section className="key-features">
+      <h2>Key Features</h2>
+      <ul>
+        <li>Immutable Records</li>
+        <li>Employer Access</li>
+        <li>Skill Tokenization</li>
+      </ul>
+    </section>
+  );
+};
+
+export default KeyFeatures;
