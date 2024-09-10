0c90556859d1acc2f382a627454e17e98b5491df
addymwenda12 <muneneaddy@gmail.com>
2024-09-10 20:17:52 +0300

    Updated App to include React Router and Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,34 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import Header from './components/Header';
-import Hero from './components/Hero';
-import AboutUs from './components/AboutUs';
-import HowItWorks from './components/HowItWorks';
-import KeyFeatures from './components/KeyFeatures';
-import SkillTokenization from './components/SkillTokenization';
-import EmployerAccess from './components/EmployerAccess';
-import Testimonials from './components/Testimonials';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import InstitutionInterface from './components/InstitutionInterface';
+import EmployerInterface from './components/EmployerInterface';
 import Login from './components/Login';
 import Register from './components/Register';
-import Dashboard from './components/Dashboard';
-import './App.css';
+import NotificationSystem from './components/NotificationSystem';
 
 function App() {
-  const [user, setUser] = useState(null);
-
   return (
-    <div>
-      <Header user={user} setUser={setUser} />
-      {user ? (
-        <Dashboard user={user} />
-      ) : (
-        <>
-          <Hero />
-          <AboutUs />
-          <HowItWorks />
-          <KeyFeatures />
-          <SkillTokenization />
-          <EmployerAccess />
-          <Testimonials />
-          <Login setUser={setUser} />
-          <Register setUser={setUser} />
-        </>
-      )}
-    </div>
+    <Provider store={store}>
+      <Router>
+        <div>
+          <Header />
+          <NotificationSystem />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/institution" component={InstitutionInterface} />
+            <Route path="/employer" component={EmployerInterface} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+          </Switch>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
