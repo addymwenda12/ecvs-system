7ebf6a5d74f2b6a4d625edec09d3d8912f1ab16a
addymwenda12 <muneneaddy@gmail.com>
2024-09-10 10:17:22 +0300

    Implemented IPFS to store larger credentials

---
+++ server/blockchain/ipfs_utils.py
@@ -0,0 +1,22 @@
+import ipfshttpclient
+
+def connect_to_ipfs():
+    """
+    Connect to IPFS
+    """
+    return ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
+
+def add_to_ipfs(data):
+    """
+    Add to IPFS
+    """
+    client = connet_to_ipfs()
+    res = client.add_json(data)
+    return res
+
+def get_from_ipfs(cid):
+    """
+    Getting from the IPFS
+    """
+    client = connect_to_ipfs()
+    return client.get_json(cid)
